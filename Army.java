12fb5660f2cfd934666f477708fccd9a3a3a4a6c
Badal kumar <s.k.badal@outlook.com>
2017-04-27 21:37:03 +0530

    algo

--- Army.java
+++ Army.java
@@ -12,32 +12,26 @@ class Army{
     // System.out.println(msg+" "+sub);
     int lmsg=arrmsg.length;
     int lsub=arrsub.length;
-    int x=0,i=0,j,k,c,d,count=0;
+
+    int i=0,j,x,count=0;
     for (j=lsub-1;j<lmsg;j++) {
-      d=0;
-      c=0;
-      k=i;
-      for(x=0;x<lsub;x++) {
-        if (arrmsg[k]==arrsub[x]) {
-          k++;
-          c++;
-          x=0;
-          d=0;
+      int c=0;
+      for (int k=i;k<lsub ;k++ ){
+        for (x=0;x<lsub ;x++ ) {
+          if (arrmsg[k]==arrsub[x]) {
+            c++;
+            break;
+          }
+          if (c==lsub) {
+            count++;
+            break;
+          }
+          if (x==lsub) {
+            i=k+1;
+            j=i+lsub;
+            break;
+          }
         }
-        else{
-          c=0;
-          d++;
-        }
-        if (c==lsub) {
-          i++;
-          j++;
-          count++;
-          break;
-        }
-      }
-      if (d==lsub) {
-        i=i+(k-i)+1;
-        j=j+(k-i)+1;
       }
     }
 System.out.println(count);
